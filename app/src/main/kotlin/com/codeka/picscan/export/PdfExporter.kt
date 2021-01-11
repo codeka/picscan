1c2a428abdedb1e538eb5961f95be945ffa91fdc
Dean Harding <dean@codeka.com.au>
2021-01-11 00:29:41 -0800

    Fixed the blank pages, but there's still a bunch of work to be done.

--- app/src/main/kotlin/com/codeka/picscan/export/PdfExporter.kt
+++ app/src/main/kotlin/com/codeka/picscan/export/PdfExporter.kt
@@ -3,6 +3,7 @@ package com.codeka.picscan.export
 import android.graphics.Paint
 import android.graphics.Rect
 import android.graphics.pdf.PdfDocument
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.codeka.picscan.App
 import com.codeka.picscan.model.Page
@@ -36,6 +37,7 @@ class PdfExporter {
       f.outputStream().use {
         doc.writeTo(it)
       }
+      doc.close()
 
       withContext(Dispatchers.Main) {
         file.value = f
@@ -54,17 +56,26 @@ class PdfExporter {
 
     val srcRect = Rect(0, 0, bmp.width, bmp.height)
     val destRect = if (srcRect.width() > srcRect.height()) {
-      val ratio = srcRect.height() / srcRect.width()
+      val ratio = srcRect.height().toDouble() / srcRect.width()
       val destHeight = canvas.width * ratio
+      Log.i(TAG, "height < width ratio=$ratio canvas.width=${canvas.width} canvas.height=${canvas.height} destHeight=$destHeight")
       Rect(
-        0, canvas.height / 2 - destHeight / 2, canvas.width, canvas.height / 2 + destHeight / 2)
+        0, (canvas.height / 2.0 - destHeight / 2.0).toInt(),
+        canvas.width, (canvas.height / 2.0 + destHeight / 2.0).toInt())
     } else {
-      val ratio = srcRect.width() / srcRect.height()
+      val ratio = srcRect.width().toDouble() / srcRect.height()
       val destWidth = canvas.height * ratio
+      Log.i(TAG, "width < height ratio=$ratio srcRect.width=${srcRect.width()} srcRect.height=${srcRect.height()} canvas.width=${canvas.width} canvas.height=${canvas.height} destWidth=$destWidth")
       Rect(
-        canvas.width / 2 - destWidth / 2, 0, canvas.width / 2 + destWidth / 2, canvas.height)
+        (canvas.width / 2 - destWidth / 2).toInt(), 0,
+        (canvas.width / 2 + destWidth / 2).toInt(), canvas.height)
     }
 
+    Log.i(TAG, "drawing bitmap: src=${srcRect.toShortString()} dest=${destRect.toShortString()}")
     canvas.drawBitmap(bmp, srcRect, destRect, Paint())
+  }
+
+  companion object {
+    private const val TAG = "PdfExporter"
   }
 }
